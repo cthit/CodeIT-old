586e35f0468dadcc9888c745587706db14592ebd
Tejp <tejpbit@hotmail.se>
2014-11-14 13:36:52 +0100

    Added fancy color error indicators on client input fields

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -3,7 +3,13 @@ package client;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Glow;
+import javafx.scene.effect.InnerShadow;
+import javafx.scene.effect.Shadow;
+import javafx.scene.paint.Color;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import network.Connection;
@@ -17,6 +23,9 @@ public class Controller {
     @FXML private TextField address;
     @FXML private TextField port;
     @FXML private TextField project_path;
+    @FXML private Label feedback_team_name;
+    @FXML private Label feedback_connection;
+    @FXML private Label feedback_project_path;
     private Connection connection;
 
     public void setStageAndDoSetup(Stage stage) {
@@ -26,11 +35,34 @@ public class Controller {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
                 if (! newValue.matches("[\\w\\d(-_.^*')]+")) {
+                    team_name.setEffect(new InnerShadow(1000, Color.DARKRED));
                     System.out.println("BAD VALUE!");
+                    feedback_team_name.setText("Plz match this -> [\\w\\d(-_.^*')]+");
+                    feedback_team_name.setTextFill(Color.DARKRED);
+                } else {
+                    team_name.setEffect(new InnerShadow(0, Color.WHITE));
+                    feedback_team_name.setText("");
+                    feedback_team_name.setTextFill(Color.GREEN);
                 }
             }
         });
 
+
+        ChangeListener<String> listener= new ChangeListener<String>() {
+
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (newValue.matches("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$")) {
+                    feedback_connection.setText("Wow very address");
+                    feedback_connection.setTextFill(Color.GREEN);
+                } else {
+                    feedback_connection.setText("Nah, plz fex.");
+                    feedback_connection.setTextFill(Color.DARKRED);
+                }
+            }
+        };
+
+        address.textProperty().addListener(listener);
     }
 
     @FXML
@@ -44,6 +76,7 @@ public class Controller {
 
     @FXML
     private void sendCodeClicked() {
+
         connection = new Connection( address.getText(), Integer.parseInt(port.getText()) );
         connection.sendMessage(team_name.getText());
         System.out.println(String.format(
