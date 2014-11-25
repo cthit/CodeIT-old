e5c30875864812a132828e4bb1dc2f4c75e59c4c
Tejp <tejpbit@hotmail.se>
2014-11-25 11:12:16 +0100

    now randomized between up and down instead of just going down

--- Server/src/game/pong_sample/PongPaddle.java
+++ Server/src/game/pong_sample/PongPaddle.java
@@ -2,13 +2,18 @@ package game.pong_sample;
 
 import game.GameMechanic;
 
+import java.util.Random;
+
 /**
  * Created by tejp on 01/11/14.
  */
 public class PongPaddle implements GameMechanic<PongGame, PongMove> {
 
+    PongMove[] pongMoves = PongMove.values();
+    Random random = new Random();
+
     @Override
     public PongMove onGameTick(PongGame game) {
-        return PongMove.DOWN;
+        return pongMoves[random.nextInt(pongMoves.length)];
     }
 }
