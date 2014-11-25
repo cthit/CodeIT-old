87af64888a95ce7be433e6161a9d7a8c666e49bc
Tejp <tejpbit@hotmail.se>
2014-11-25 11:05:31 +0100

    Added iterator class for iterating through rounds

--- Server/src/game/Model.java
+++ Server/src/game/Model.java
@@ -1,10 +1,15 @@
 package game;
 
+import com.sun.xml.internal.ws.addressing.model.ActionNotSupportedException;
+import game.pong_sample.PongGame;
+import game.pong_sample.PongMove;
 import game.view.NewGameListener;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 
 /**
  * Created by tejp on 01/11/14.
@@ -31,6 +36,15 @@ public class Model<T, M> {
         this.newGameListener = newGameListener;
     }
 
+    public void createNewGame(Competitor<T,M> competitor1, Competitor<T,M> competitor2) {
+        game = gameFactory.apply(competitor1, competitor2);
+    }
+
+    public void gameLoop() {
+        if ( ! game.isGameOver())
+            game.play();
+    }
+
     public void play() {
         for (Competitor<T, M> player1 : competitors) {
             for (Competitor<T, M> player2 : competitors) {
@@ -49,5 +63,55 @@ public class Model<T, M> {
 
     public Game getGame() {
         return game;
+    }
+
+
+    public static class Round<T,M> {
+        private final Competitor<T,M> competitor1;
+        private final Competitor<T,M> competitor2;
+
+        public Round(Competitor<T,M> competitor1, Competitor<T,M> competitor2) {
+            this.competitor1 = competitor1;
+            this.competitor2 = competitor2;
+        }
+
+
+    }
+    public static class RoundIterator<T,M> implements Iterator<Round> {
+
+        private List<Competitor<T,M>> competitors = new ArrayList<>();
+        private int competitor1 = 0;
+        private int competitor2 = 1;
+
+        public RoundIterator(List<Competitor<T,M>> competitors) {
+            this.competitors = competitors;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return competitor1 < competitors.size() - 1 ;
+        }
+
+        @Override
+        public Round next() {
+            Round round = new Round(competitors.get(competitor1), competitors.get(competitor2));
+
+            if (competitor2 >= competitors.size() - 1) {
+                competitor1++;
+                competitor2 = competitor1;
+            }
+            competitor2++;
+            return round;
+        }
+
+        @Override
+        public void remove() {
+            throw new ActionNotSupportedException("Can't remove Round from iterator");
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super Round> action) {
+
+        }
     }
 }
