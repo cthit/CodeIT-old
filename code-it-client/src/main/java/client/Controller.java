6d1c574aa72184f87312ff4d605a6d8e6b8918c8
Tejp <tejpbit@hotmail.se>
2014-12-28 19:03:00 +0100

    user now need to find file path in client, not project path.

--- code-it-client/src/main/java/client/Controller.java
+++ code-it-client/src/main/java/client/Controller.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.InnerShadow;
 import javafx.scene.paint.Color;
-import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import network.Connection;
 
@@ -23,7 +23,7 @@ public class Controller {
     @FXML private TextField team_name;
     @FXML private TextField address;
     @FXML private TextField port;
-    @FXML private TextField project_path;
+    @FXML private TextField file_path;
     @FXML private Label feedback_team_name;
     @FXML private Label feedback_connection;
     @FXML private Label feedback_project_path;
@@ -68,11 +68,11 @@ public class Controller {
 
     @FXML
     private void browseClicked() {
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        directoryChooser.setTitle("Open Project Folder");
-        File file = directoryChooser.showDialog(stage);
-        if (file == null)
-            return;
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open your AI file");
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null)
+            file_path.setText(file.getAbsolutePath());
     }
 
     @FXML
