09f3b805e44d7d9659a8f2bed4da152bab3e570b
Mattias Nilsen <kerp@chalmers.it>
2015-02-15 14:25:26 +0100

    Server can now compile files that client send to it.

--- code-it-server/src/main/java/game/ServerController.java
+++ code-it-server/src/main/java/game/ServerController.java
@@ -8,8 +8,11 @@ import network.ServerConnection;
 import pong_sample.PongGame;
 import pong_sample.PongMove;
 import pong_sample.PongPaddle;
+import utils.JavaSourceFromString;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.BiFunction;
@@ -65,14 +68,24 @@ public class ServerController implements NewFileFromClientListener {
 
     @Override
     public void newFileRecieved(String teamName, File f) {
-        //TODO create gameMechanic (i.e pongPaddle) from file
-        GameMechanic<PongGame,PongMove> pongPaddle = new PongPaddle();
-
-        executor.submit(() -> {
-            model.handleContributionFromCompetitor(teamName, pongPaddle);
-        });
-        System.out.println("Notified of new file in ServerControll. Fance");
-
+        String code = null;
+        try {
+            code = new String(Files.readAllBytes(f.toPath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        code = code.replaceFirst("package\\s+.+?;", "package pong_sample;");
+        code = code.replaceFirst("public\\s+class\\s+.+?\\s", "public class " + teamName + " ");
+        try {
+            GameMechanic<PongGame,PongMove> pongPaddle = (GameMechanic<PongGame,PongMove>) JavaSourceFromString.compile(code, teamName + ".java", "pong_sample");
+            pongPaddle.createTestGame();
+            executor.submit(() -> {
+                model.handleContributionFromCompetitor(teamName, pongPaddle);
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
