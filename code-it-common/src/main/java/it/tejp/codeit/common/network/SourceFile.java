4e1d6dbf4e26a27ae376e4ff4d8924059db6f9f6
André Samuelsson <andresamuelsson94@gmail.com>
2015-10-19 17:45:57 +0200

    Add new common file for transfering java File object

---
+++ code-it-common/src/main/java/it/tejp/codeit/common/network/SourceFile.java
@@ -0,0 +1,15 @@
+package it.tejp.codeit.common.network;
+
+import java.io.File;
+
+/**
+ * Created by tejp on 19/10/15.
+ */
+public class SourceFile {
+
+    public File file;
+
+    public SourceFile(File file) {
+        this.file = file;
+    }
+}
