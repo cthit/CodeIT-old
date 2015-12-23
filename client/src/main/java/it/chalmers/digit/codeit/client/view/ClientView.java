f4202408dab600ed02885a0240d702f63dea9ef9
Kerp <kerp@chalmers.it>
2015-12-23 15:08:39 +0100

    ClientView start method throws IOException instead of Exception

--- client/src/main/java/it/chalmers/digit/codeit/client/view/ClientView.java
+++ client/src/main/java/it/chalmers/digit/codeit/client/view/ClientView.java
@@ -7,13 +7,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class ClientView extends Application {
 
     private final int MIN_HEGTH = 800;
     private final int MIN_WIDTH = 825;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
         Parent root = loader.load();
         primaryStage.setTitle("CodeIT");
