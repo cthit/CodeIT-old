65e7d94e015d1229cc288f18f0330be8815a42ce
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-01-24 15:25:22 +0100

    Handle network with kryonet in ratingvisualizer

--- ratingvisualizer/src/main/java/it/chalmers/digit/codeit/ratingvisualizer/RatingVisualizerController.java
+++ ratingvisualizer/src/main/java/it/chalmers/digit/codeit/ratingvisualizer/RatingVisualizerController.java
@@ -1,5 +1,9 @@
 package it.chalmers.digit.codeit.ratingvisualizer;
 
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import it.chalmers.digit.codeit.common.network.*;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -7,10 +11,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import org.controlsfx.dialog.Dialogs;
+import org.apache.commons.lang3.ArrayUtils;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Map;
 import java.util.concurrent.Executors;
@@ -21,7 +27,8 @@ import java.util.logging.Logger;
 /**
  * Created by tejp on 16/02/15.
  */
-public class RatingVisualizerController {
+@SuppressWarnings("Duplicates")
+public class RatingVisualizerController extends Listener {
 
     private static final Logger log = Logger.getLogger(RatingVisualizer.class.getName());
 
@@ -32,76 +39,150 @@ public class RatingVisualizerController {
 
     private static final ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(1);
     @FXML private TableView table;
+    private Stage stage;
+
+    private Client client = null;
+    private byte[] chunks = null;
+    Thread clientThread = null;
+    private int chunkSize = -1; //Chunk size -1 indicates that currently no chunk transfer is in progress.
+
+    public void initNetwork() {
+        client = new Client(Network.BUFFER_SIZE, Network.BUFFER_SIZE);
+
+        Initializer.registerClasses(client.getKryo());
+        client.addListener(this);
+        clientThread = new Thread(client);
+        clientThread.start();
+        try {
+            client.connect(5000, adress, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void connected(Connection connection) {
+        log.info("Connected to " + connection.getRemoteAddressTCP());
+    }
+
+    /**
+     * Callback when data is received on a connection.
+     * @param connection The connection on which the data is received .
+     * @param object The data received.
+     */
+    @Override
+    public void received(Connection connection, Object object) {
+        if(object instanceof MessageWithObject) {
+            handleMessageWithObject((MessageWithObject) object);
+        }
+    }
+
+    private void handleMessageWithObject(MessageWithObject msg) {
+        log.info("Received: " + msg.message);
+        if(msg.message == Message.CHUNKED_TRANSFER) {
+            handleNewChunkedTransfer(msg);
+        } else if(msg.message == Message.CHUNK) {
+            handleChunk(msg);
+        } else if(msg.message == Message.TRANSFER_RATINGS) {
+            handleTransferRatings(msg);
+        }
+    }
+
+    private void handleTransferRatings(MessageWithObject msg) {
+        final ObservableList items = table.getItems();
+
+        Platform.runLater(() -> {
+            items.clear();
+            Map<String, Double> ratings = (Map<String, Double>)msg.object;
+            ratings.entrySet().forEach(e -> {
+                items.add(new TeamRatingEntry(
+                        e.getKey(),
+                        e.getValue()
+                ));
+            });
+        });
+    }
+
+    private void handleChunk(MessageWithObject msg) {
+        if(chunkSize == -1) {
+            Platform.runLater(() -> errorDialog("Transfer error", "", "Got chunk while no chunk transfer was in progress"));
+        } else {
+            chunks = ArrayUtils.addAll(chunks, (byte[])msg.object);
+            log.info("Chunksize: " + chunks.length);
+            if(chunks.length == chunkSize) {
+                log.info("chunks.length == chunkSize");
+                try {
+                    chunkSize = -1;
+                    handleMessageWithObject((MessageWithObject) Serializer.deserialize(chunks));
+                } catch (IOException e) {
+                    Platform.runLater(() -> errorDialog("Transfer error", e.getMessage(), "Couldn't deserialize a received message"));
+                } catch (ClassNotFoundException e) {
+                    Platform.runLater(() -> errorDialog("Transfer error", e.getMessage(), "Couldn't deserialize chunked transfer in a meaningfull way"));
+                }
+            } else if(chunks.length > chunkSize) {
+                Platform.runLater(() -> errorDialog("Transfer error", "Received too many bytes in a chunked transfer", "chunks.length > chunkSize"));
+            }
+        }
+    }
+
+    /**
+     * Initiates a new chunk transfer, only one chunk transfer can can happen at a time.
+     * @param msg The message containing the expected size of all the chunks.
+     */
+    private void handleNewChunkedTransfer(MessageWithObject msg) {
+        log.info("handleNewChunkedTransfer");
+        if(chunkSize == -1) {
+            chunkSize = (int)msg.object;
+            chunks = null;
+        } else {
+            Platform.runLater(() -> errorDialog("Transfer error", "", "New chunked transfer initiated while one was already in progress"));
+            client.close();
+        }
+    }
+
+    /**
+     * Callback when a connection is disconnected.
+     * @param connection The connection that was disconnected.
+     */
+    @Override
+    public void disconnected(Connection connection) {
+        log.info("Disconnected " + connection.getRemoteAddressTCP());
+    }
 
     public void startDataWatcher() {
-        System.out.println(table.toString());
-
-        data = table.getItems();
-
-        scheduledExecutor.scheduleAtFixedRate(() -> {
-            final ObservableList items = table.getItems();
-
-            Platform.runLater(() -> {
-                items.clear();
-                Map<String, Double> ratings = requestRatingFromServer();
-                ratings.entrySet().forEach(e -> {
-                    items.add(new TeamRatingEntry(
-                            e.getKey(),
-                            e.getValue()
-                    ));
-                });
-            });
-
-        }, 0, 5, TimeUnit.SECONDS);
-
-
-
+        scheduledExecutor.scheduleAtFixedRate(this::requestRatingFromServer, 0, 5, TimeUnit.SECONDS);
     }
 
-    public Map<String, Double> requestRatingFromServer() {
-        Socket socket = null;
-        try {
-            socket = new Socket(adress, port);
-            OutputStream os = socket.getOutputStream();
-            BufferedOutputStream bos = new BufferedOutputStream(os);
-            byte[] byteMessage = "RequestRating".getBytes();
-            for (int i = 0 ; i < byteMessage.length ; i++) {
-                bos.write(byteMessage[i]);
-            }
-            bos.flush();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //**************************************************************************************************************
-
-
-        BufferedInputStream bis;
-        ObjectInputStream ois;
-        Map<String,Double> receivedRatings = null;
-        try {
-            bis = new BufferedInputStream(socket.getInputStream());
-            ois = new ObjectInputStream(bis);
-
-            receivedRatings =  (Map<String, Double>)ois.readObject();
-
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return receivedRatings;
+    public void requestRatingFromServer() {
+        client.sendTCP(Message.REQUEST_RATINGS);
     }
 
-    public void initialize(String address, int port) {
+    public void initialize(String address, int port, Stage stage) {
         this.port = port;
+        this.stage = stage;
         try {
             this.adress = InetAddress.getByName(address);
         } catch (UnknownHostException e) {
             throw new RuntimeException(String.format("Could not find host ip address: %s", address));
         }
+        initNetwork();
+        startDataWatcher();
+    }
+
+    /**
+     * Displays an dialog box with the error template to the user.
+     * Needs to be invoked with Platform.runlater(() -> ) in the network thread.
+     * @param title The title of the dialog box.
+     * @param masthead The masthead dof the dialog box.
+     * @param message The message of the dialog box.
+     */
+    private void errorDialog(String title, String masthead, String message) {
+        Dialogs.create()
+                .owner(stage)
+                .title(title)
+                .masthead(masthead)
+                .message(message)
+                .showError();
     }
 
     public static class TeamRatingEntry {
